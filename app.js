3e24f5d03d627b09345290cf36b5f807236f5bc5
Maniprabha06 <prabha6603@gmail.com>
2025-04-12 19:05:57 +0530

    Add CI pipeline

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const name = "PrabhaTest";
 
 app.get("/", (req,res) => {
-    res.send("Welcome to Buylio from Prabha!");
+    const message = `Welcome to Buylio from ${name}!`
+    res.send(message);
 })
 
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
 
-module.exports = {app, server}
+module.exports = {app, server, name}
